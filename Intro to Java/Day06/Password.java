6f11e572a6212ad88850f2f7bd22e7b8993ac189
Alex Langlois <60579219+AlexLanglois-lab@users.noreply.github.com>
2020-02-03 20:20:05 -0600

    Update Password.java

--- Intro to Java/Day06/Password.java
+++ Intro to Java/Day06/Password.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication1;
+
+package javaapplication18;
 
 import java.util.Random;
 
-public class JavaApplication1 {
+public class JavaApplication18 {
 
     public static void main(String[] args) {
         Random random = new Random();
