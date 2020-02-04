a68bdf064178a484e36c09ed30291231010294cd
Alex Langlois <60579219+AlexLanglois-lab@users.noreply.github.com>
2020-02-03 19:56:04 -0600

    Update GradeLoop.java

--- Intro to Java/Day04/GradeLoop.java
+++ Intro to Java/Day04/GradeLoop.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication94;
+package javaapplication3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Schroeder <schroeder.austincc.edu>
- */
 public class JavaApplication94 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char[] characters = new char[20];
