11687235ec37b877908a569b4a928f35b3701ef7
Alex Langlois <60579219+AlexLanglois-lab@users.noreply.github.com>
2020-02-03 20:17:49 -0600

    Update Encryption.java

--- Intro to Java/Day06/Encryption.java
+++ Intro to Java/Day06/Encryption.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication3;
 
-/**
- *
- * @author javauser
- */
-public class JavaApplication3 {
+package javaapplication15;
 
-    /**
-     * @param args the command line arguments
-     */
+public class JavaApplication15 {
+
     public static void main(String[] args) {
 //        char[] message = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o',
 //        'r', 'l', 'd', '!'};
